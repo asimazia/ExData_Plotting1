e7db916ed118ff23f1d6de9dffacdd9bf8ddc4e0
asimazia <46671890+asimazia@users.noreply.github.com>
2019-06-27 19:04:04 +0300

    Exploratory Data Analysis Assignment1

--- Plot3.R
+++ Plot3.R
@@ -1,12 +1,12 @@
 file <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?")
 data <- subset(file, Date %in% c("1/2/2007","2/2/2007"))
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
-datatime <- paste(as.Date(data$Date), data$Time)
-data$datatime <- as.POSIXct(datatime)
+datatime <- paste(as.Date(data$Date), data$Time) # I used paste and POSIXct instead of strptime() as I find it easier than the other one.
+data$datatime <- as.POSIXct(datatime) # Also datatime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") can be used
 with(data, {plot(Sub_metering_1 ~ datatime, type = "l", ylab="Energy sub metering", xlab ="")
      lines(Sub_metering_2 ~ datatime,col='Red')
      lines(Sub_metering_3 ~ datatime,col='Blue')})
 legend("topright", col=c("black", "red", "blue"), lwd=2, lty=1,
         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.copy(png, file="plot3.png")
+dev.copy(png, file="plot3.png", height = 480, width = 480)
 dev.off()
