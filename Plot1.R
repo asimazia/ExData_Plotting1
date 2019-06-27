8854efb9090c19c50941da5127c66c09ff14650b
asimazia <46671890+asimazia@users.noreply.github.com>
2019-06-27 19:16:17 +0300

    Exploratory Data Analysis Assignment1

--- Plot1.R
+++ Plot1.R
@@ -5,5 +5,5 @@ data <- subset(file, Date %in% c("1/2/2007","2/2/2007"))
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 gap <- data$Global_active_power
 hist(gap, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-dev.copy(png, file="plot1a.png")
+dev.copy(png, file="plot1.png", height = 480, width = 480)
 dev.off()
